ed8feb8b20fcfd77bedfb4cfd4bf98f5973792e8
Daniel Sanoubane <ddsanoubane@myseneca.ca>
2024-02-12 02:41:19 -0500

    Lastest Update via Branch

--- Lab1.py
+++ Lab1.py
@@ -2,12 +2,22 @@
 #Description of Function: A function that takes an input from the user and calculate someone's age. 
 
 def age():
-    year = int(input("Enter year of birth: "))
+    try:
+        year = int(input("Enter year of birth: "))
 
-    currentyear = 2024
+        currentyear = 2024
 
-    age = currentyear - year
+        age = currentyear - year
 
-    print('You are ' + str(int(age)) + ' years old.')
+        print('You are ' + str(int(age)) + ' years old.')
+    
+    except ValueError:
+         print('Please enter an int.')
 
 age()
+
+
+def helloWorld():
+	print('Hello World')
+
+helloWorld()
