6f1914e2fba34f391ae0ec7cfe4c7ec431269279
ddsanoubane <151870714+ddsanoubane@users.noreply.github.com>
2024-02-11 23:10:58 -0500

    Create Lab1.py

---
+++ Lab1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#Description of Function: A function that takes an input from the user and calculate someone's age. 
+
+def age():
+    year = int(input("Enter year of birth: "))
+
+    currentyear = 2024
+
+    age = currentyear - year
+
+    print('You are ' + str(int(age)) + ' years old.')
+
+age()
